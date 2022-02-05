2747818e0a5d04d9c214761039900d17c1085c1e
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-02-05 00:29:35 -0800

    Park

---
+++ assignments/assignment2-2.cpp
@@ -0,0 +1,18 @@
+//Jonathan Park
+//Professor Lee
+//CS 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter;
+
+    letter=65;
+    cout << letter << endl;
+    letter=66;
+    cout << letter << endl;
+    return 0;
+}
+
+//Character values are stored in memory by using ACSII, which assigns a character with a numeric code.
