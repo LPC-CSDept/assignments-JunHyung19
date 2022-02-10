18fb6bb9cc9eee558fb83e197b23d45981bd20c4
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-02-10 10:34:10 -0800

    Park

---
+++ assignments/assignment3-1.cpp
@@ -0,0 +1,16 @@
+//Jonathan Park
+//Professor Lee
+//CS 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter;
+
+    letter=65;
+    cout << letter << endl;
+    letter=66;
+    cout << letter << endl;
+    return 0;
+}
