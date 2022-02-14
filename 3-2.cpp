3f10e6558732586771ab2a10a6ba2eee7749fad2
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-02-14 02:59:33 -0800

    3-2 cpp

--- 3-2.cpp
+++ 3-2.cpp
@@ -2,29 +2,25 @@
 //Professor Lee
 //CS 1
 #include <iostream>
-#include<string>
-#include <cmath> 
-
+#include <cmath>
 using namespace std;
 
 
 int main()
 {
-    int numM;
-    int numF;
+    int numM,numF;
+    int total;
+    double perM,perF;
 
     cout << "Enter number of male students in class: ";
     cin >> numM;
     cout << "Enter number of female students in class: ";
     cin >> numF;
 
-    int total;
     total = numM+numF;
 
-    float perM;
-    float perF;
-    perM = (numM/total)*100;
-    perF = (numF/total)*100;
+    perM = 100*numM/total;
+    perF = 100*numF/total;
     
     cout << "Percentage of male students: " << perM;
     cout << "%.\n";
