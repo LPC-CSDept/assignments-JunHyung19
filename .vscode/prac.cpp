01408a8f94b5fa9b878b14e3bf4e63a7bed0d180
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-02-24 00:16:29 -0800

    3-3

---
+++ .vscode/prac.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    int numM,numF;
+    int total;
+    float perM,perF;
+
+    cout << "Enter number of male students in class: ";
+    cin >> numM;
+    cout << "Enter number of female students in class: ";
+    cin >> numF;
+
+    total = numM+numF;
+
+    perM = 100.0*numM/total;
+    perF = 100.0*numF/total;
+    
+    cout << "Percentage of male students: " << perM;
+    cout << "%.\n";
+    cout << "Percentage of female students: " << perF;
+    cout << "%.\n";
+   
+    return 0; 
+
+}
---
+++ 3-3.cpp
@@ -0,0 +1,15 @@
+//Jonathan Park
+//Professor Lee
+//CS 1
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double C, F;
+
+    cout << "Enter the temperature by Celcius";
+    
+
+}
