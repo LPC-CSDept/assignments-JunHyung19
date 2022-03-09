77ef2cc8ef638c8fc9245d5aee3f61411457296a
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-03-08 19:31:48 -0800

    Quiz3

--- Quiz3/Quiz3.cpp
+++ Quiz3/Quiz3.cpp
@@ -2,10 +2,16 @@
 //Professor Lee
 //CS 1
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
+    double seed = time(0);
+    srand(seed);
+
+    double num1, num2, num3;
 
     return 0;
 }
