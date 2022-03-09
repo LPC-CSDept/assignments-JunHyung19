41faf0a0d3cd99e5fa0249295244b9dc3e4fc2bd
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-03-08 19:35:33 -0800

    Quiz3

--- Quiz3/Quiz3.cpp
+++ Quiz3/Quiz3.cpp
@@ -12,6 +12,19 @@ int main()
     srand(seed);
 
     double num1, num2, num3;
+    num1=rand()%100;
+    num2=rand()%100;
+    num3=rand()%100;
 
+    double sum;
+    sum=num1+num2+num3;
+
+    double avg;
+    avg=sum/3;
+
+    cout << "First random number: " << num1 << endl;
+    cout << "Second random number: " << num2 << endl;
+    cout << "Third random number: " << num3 << endl;
+    
     return 0;
 }
