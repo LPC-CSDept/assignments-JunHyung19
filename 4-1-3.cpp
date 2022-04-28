bd3be548cce6bc9fe1e5e90d65a413ac569c0ea0
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-04-27 19:18:32 -0700

    4-1-3

--- 4-1-3.cpp
+++ 4-1-3.cpp
@@ -7,5 +7,47 @@ using namespace std;
 int main()
 
 {
-    int 
+    double pw, d;
+    double r1, r2, r3, r4;
+    double rate;
+
+    r1=1.10;
+    r2=2.20;
+    r3=3.70;
+    r4=4.80;
+
+    cout<<"Enter package weight in kilograms: ";
+    cin>>pw;//weight of package
+    cout<<"Enter shipping distance in miles: ";
+    cin>>d;//distance
+
+    if (pw<0)
+    exit(0);
+    if (pw>20)
+    exit(0);
+    if (d<0)
+    exit(0);
+    if (d>3000)
+    exit(0);
+
+    if (pw<=2)
+    rate=r1;
+    else if (pw<=6)
+    rate=r2;
+    else if (pw<=10)
+    rate=r3;
+    else
+    rate=r4;
+
+    double price;
+
+    if (d<500)
+    price=rate;
+    else
+    price=d/500*rate;
+
+    cout<<"Price: "<<price<<endl;
+    cout<<"Distance: "<<d<<endl;
+    cout<<"Weight: "<<pw<<endl;
+
 }
