420dce432af671f3dae3997e825b8e016c0ce37f
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-04-30 16:51:42 -0700

    4-2

--- 4-2.cpp
+++ 4-2.cpp
@@ -7,5 +7,42 @@ using namespace std;
 int main()
 
 {
+    int num1, num2, num3;
+    int max, mid, min;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    cout << "Enter the third number: ";
+    cin >> num3;
     
+    {
+        if (num1>num2 && num1>num3)
+        max=num1;
+        else if (num1<num2 && num1<num3)
+        min=num1;
+        else 
+        mid=num1;
+    }
+    
+    {
+        if (num2>num1 && num2>num3)
+        max=num2;
+        else if (num2<num1 && num2<num3)
+        min=num2;
+        else 
+        mid=num2;
+    }
+
+    {
+        if (num3>num1 && num3>num2)
+        max=num3;
+        else if (num3<num1 && num3<num2)
+        min=num3;
+        else 
+        mid=num3;
+    }
+
+    cout<<"Your numbers in ascending order: ("<<min<<" "<<mid<<" "<<max<<")"<<endl;
 }
