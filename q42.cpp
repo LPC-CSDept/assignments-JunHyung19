755e1bc64d4e04db463ff86dc1668e26555f58c7
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-01 01:40:31 -0700

    q42

--- q42.cpp
+++ q42.cpp
@@ -10,40 +10,35 @@ int main()
     double pw, d;
     double r1, r2, r3, r4;
     double rate;
+    double price;
 
     r1=1.10;
     r2=2.20;
     r3=3.70;
     r4=4.80;
 
-    cout<<"Enter package weight in kilograms: ";
+    cout<<"Enter the package weight in kilograms: ";
     cin>>pw;//weight of package
-        if (pw<0)
+        if (pw<=0||pw>20)
             exit(0);
-        if (pw>20)
-            exit(0);
-    cout<<"Enter shipping distance in miles: ";
+    cout<<"Enter the shipping distance in miles: ";
     cin>>d;//distance
-        if (d<10)
-            exit(0);
-        if (d>3000)
+        if (d<10||d>3000)
             exit(0);
 
     if (pw<=2)
-    rate=r1;
+        rate=r1;
     else if (pw<=6)
-    rate=r2;
+        rate=r2;
     else if (pw<=10)
-    rate=r3;
+        rate=r3;
     else
-    rate=r4;
-
-    double price;
+        rate=r4;
 
     if (d<=500)
-    price=rate;
+        price=rate;
     else
-    price=d/500*rate;
+        price=d/500*rate;
 
     cout<<"Price: $"<<price<<endl;
     cout<<"Distance: "<<d<<" miles"<<endl;
