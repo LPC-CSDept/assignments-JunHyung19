f4aae781d90df3a9ba647bac98ce9728abdebb00
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 08:54:36 -0700

    Quiz 5

--- q5-2-4-read.cpp
+++ q5-2-4-read.cpp
@@ -12,7 +12,21 @@ int main()
     int id;
     string nam;
     string dep;
-    double sal,tot,avg;
+    double sal,tot,avg;//tot=total, avg=average
+
+    ifs.open("employee.txt");
+    if (!ifs)
+    {
+        cout<<"Error"<<endl;
+        exit(0);
+    }
+    ifs>>num;
+
+    for(int i=0;i<num;i++)
+    {
+        ifs>>id>>nam>>dep>>sal;
+
+    }
 
 
 }
