b65dddd8b015cddc22cfec83cdc0ebb9a74eca56
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 15:14:38 -0700

    q6

--- Quiz6/q6-2.cpp
+++ Quiz6/q6-2.cpp
@@ -3,6 +3,8 @@
 //CS 1
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 void gerRandnum(int &n1, int &n2, int &n3);
@@ -10,5 +12,6 @@ void gerRandnum(int &n1, int &n2, int &n3);
 int main()
 
 {
-
+    int n1,n2,n3;
+    cout<<n1<<" "<<n2<<" "<<n3;
 }
