329eddc3856317c4329855bad5a3e68f63279a83
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 14:36:03 -0700

    Quiz6

--- Quiz6/q6-1.cpp
+++ Quiz6/q6-1.cpp
@@ -5,11 +5,12 @@
 #include <iomanip>
 using namespace std;
 
-void swapTwoValues(int n1, int n2);
-void swapTwoValues(float f1, float f2);
+void swap(int n1, int n2);
+void swap(float f1, float f2);
 
 int main()
 
 {
-    
+    int n1,n2;
+
 }
