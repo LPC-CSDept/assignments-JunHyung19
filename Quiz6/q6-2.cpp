34bc9695ee897359df668c752c2615f831e4ea95
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 15:23:14 -0700

    q6

--- Quiz6/q6-2.cpp
+++ Quiz6/q6-2.cpp
@@ -10,9 +10,19 @@ using namespace std;
 void gerRandnum(int &n1, int &n2, int &n3);
     double seed = time(0);
     srand(seed);
-    n1=rand()%100;
-    n2=rand()%100;
-    n3=rand()%100;
+    int n1=rand()%100;
+    int n2=rand()%100;
+    int n3=rand()%100;
+
+int  findMin(int n1, int n2, int n3);
+    {
+        if (n1<n2 && n1<n3)
+        min=num1;
+        else if (n2<n1 && n2<n3)
+        min=num2;
+        else if (n3<n1 && n3<n2)
+        min=num3;
+    }
     
 
 int main()
