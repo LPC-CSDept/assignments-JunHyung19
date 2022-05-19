56e97ab191be94105c11811433b75465a03e8f70
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 08:31:45 -0700

    Quiz 5 #4

--- q5-2-4.cpp
+++ q5-2-4.cpp
@@ -7,5 +7,16 @@ using namespace std;
 
 int main()
 {
-    
+    ofstream ofs;
+    int num;//number of employees
+    int id;//ID
+    string nam;//employee name
+    string dep;//department
+    double sal;//salary
+
+    ofs.open("employee.txt");
+    if (!ofs)
+    {
+        
+    } 
 }
