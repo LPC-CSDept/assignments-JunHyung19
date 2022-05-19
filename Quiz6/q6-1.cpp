e3cab1360bff841e7bfbf958524daf3979d59d20
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 14:50:06 -0700

    Quiz6

--- Quiz6/q6-1.cpp
+++ Quiz6/q6-1.cpp
@@ -12,8 +12,12 @@ int main()
 
 {
     int n1,n2;
+    float f1,f2;
 
-    cout<<"Input n1 and n2: "
-    cin>>
+    cout<<"Input n1 and n2: "<<endl;
+    cin>>n1>>n2;
+    swap(n1,n2);
+
+    
 
 }
