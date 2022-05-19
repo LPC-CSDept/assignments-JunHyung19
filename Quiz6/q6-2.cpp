82fb398a0222275f4b9a58c3784cba9d9d709963
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 15:19:11 -0700

    q6

--- Quiz6/q6-2.cpp
+++ Quiz6/q6-2.cpp
@@ -8,10 +8,16 @@
 using namespace std;
 
 void gerRandnum(int &n1, int &n2, int &n3);
+    double seed = time(0);
+    srand(seed);
+    n1=rand()%100;
+    n2=rand()%100;
+    n3=rand()%100;
+    
 
 int main()
 
 {
-    int n1,n2,n3;
+
     cout<<n1<<" "<<n2<<" "<<n3;
 }
