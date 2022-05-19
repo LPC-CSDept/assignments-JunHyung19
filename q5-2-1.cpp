21ecaa219905cebdbfa7c4201c3b5ce147974f9b
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 07:04:08 -0700

    Quiz 5

--- q5-2-1.cpp
+++ q5-2-1.cpp
@@ -11,6 +11,9 @@ int main()
     srand(time(0));
 
     int n1,n2,n3,n4,n5;
+    int min;
+
+
     n1=rand()%100;
     n2=rand()%100;
     n3=rand()%100;
@@ -18,4 +21,5 @@ int main()
     n5=rand()%100;
 
     cout<<n1<<", "<<n2<<", "<<n3<<", "<<n4<<", "<<n5<<endl;
+
 }
