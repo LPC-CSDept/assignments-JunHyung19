32513c9f83f6111a8dfc80d5eefa0b737cdf38d4
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 19:19:29 -0700

    Midterm

--- Midterm/Midterm.cpp
+++ Midterm/Midterm.cpp
@@ -2,11 +2,16 @@
 //Professor Lee
 //CS 1
 #include <iostream>
-#include <string>
 using namespace std;
 
 
 int main()
 {
-    
+    const  int N=5, M=5;
+    int     i, j;
+    for(i=0;i<N;i++) {
+        for(j=i; j<M-i; j++)
+            cout << " (" << i << "," << j << ") " ;
+        cout << endl;
+    }
 }
