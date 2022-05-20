8ac9231a21d3878696beb4c650046ba5ae0f759a
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 21:46:53 -0700

    midterm

--- Midterm/m4.cpp
+++ Midterm/m4.cpp
@@ -11,24 +11,26 @@ using namespace std;
 int getRdnum()
 {
     srand(time(NULL));
-    return rand()%101;
+    return rand()%101;//to get a random number from 1 to 100
 }
 
 int getRdnum(int n1)
 {
     srand(time(NULL));
-    return rand()%(101-n1)+n1;
+    return rand()%(101-n1)+n1;//to get a random number from 1 to n2
 }
 
 int getRdnum(int n1,int n2)
 {
     srand(time(NULL));
-    return rand()%(n2-n1)+n1;    
+    return rand()%(n2-n1)+n1;//to get a random number from n1+1 to n2
 }
 
 int main()
 {
+    int n1,n2;
     cout<<getRdnum()<<endl;
-    cout<<getRdnum()<<endl;
-    cout<<getRdnum()<<endl;    
+    cout<<getRdnum(n1)<<endl;
+    cout<<getRdnum(n1,n2)<<endl;    
 }
+//numbers are randomized but first two match
