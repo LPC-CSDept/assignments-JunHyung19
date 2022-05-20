4028ae52db4815ee25ebaddbbc2f110486ba6564
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 22:10:34 -0700

    midterm

--- Midterm/m5.cpp
+++ Midterm/m5.cpp
@@ -19,7 +19,7 @@ int isSame(int&n1,int&n2)//to check if the numbers are the same, which shouldn't
     return 1;
 }
 
-double intDivision(int&n1,int&n2)
+double intDivision(int&n1,int&n2)//to see which number should be divided by the other number
 {
     if(n1>n2)
         return n1/n2;
@@ -27,9 +27,24 @@ double intDivision(int&n1,int&n2)
         return n2/n1;
 }
 
+void fileWrite(double result)
+{
+    ofstream ofs;
+}
+
 int main()
 {
+    ofstream ofs;
     int n1,n2;
     double result;
-    
+
+    ofs.open("question5.txt");
+    if (!ofs)
+    {
+        cout<<"Error"<<endl;
+        exit(0);
+    }
+
+    getInput(n1,n2);
+
 }
