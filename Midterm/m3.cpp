483a4ac3ec192714236f85d71c657f97eac2604f
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 20:59:19 -0700

    midterm

--- Midterm/m3.cpp
+++ Midterm/m3.cpp
@@ -3,13 +3,25 @@
 //CS 1
 #include <iostream>
 #include <fstream>
-#include <iomanip>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
+int getRdnum(void);
 
 int main()
 {
-    
+    ofstream ofs;
+    int N=10;
+    int i;
+
+    ofs.open("numbers.txt");
+    if (!ofs)
+    {
+        cout<<"Error"<<endl;//error pops up when unable to comply and stops code
+        exit(0);
+    }
+    for(i=0;i<10;i++)
+    {
+
+    }
+    ofs.close( );
 }
