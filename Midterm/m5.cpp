e73c590bbbd54393f16a55bf455529657da33dbe
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-19 22:02:19 -0700

    midterm

--- Midterm/m5.cpp
+++ Midterm/m5.cpp
@@ -5,13 +5,21 @@
 #include <fstream>
 using namespace std;
 
-void getInput(int&n1,int&n2)
+void getInput(int&n1,int&n2)//to get the two user input numbers
 {
     cout<<"Input your first number: ";
     cin>>n1;
     cout<<"Input your second number: ";
     cin>>n2;
 }
+
+int isSame(int&n1,int&n2)//to check if the numbers are the same, which shouldn't be the case
+{
+    if(n1=n2)
+    exit;
+}
+
+int intDivision(int&n1,int&n2)
 
 int main()
 {
