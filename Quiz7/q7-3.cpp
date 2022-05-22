c1afe2f698851026ecc770763fda2ec05d0aa63b
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-22 03:36:24 -0700

    Quiz7

--- Quiz7/q7-3.cpp
+++ Quiz7/q7-3.cpp
@@ -7,8 +7,14 @@ int findmax(int number[], int from, int to)
 {
     int max=number[from];
     for(int i=from+1;i<=to;i++)
+    {
+        if(max<number[i])
+        {
+            max=number[i];
+        }
+    }
+    return max;
 }
-
 
 const int N = 10;
 
