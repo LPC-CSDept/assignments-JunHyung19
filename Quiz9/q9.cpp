76a142bd636bcacfd946b3702b1471f13e909d7d
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-23 13:18:54 -0700

    Quiz9

--- Quiz9/q9.cpp
+++ Quiz9/q9.cpp
@@ -3,9 +3,20 @@
 //CS 1
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
+
+struct employee
+{
+    int id;//ID number
+    string name;//employee name
+    double sal;//annual salaries
+    string dep;//department name
+    string date;//starting work date
+}
+
 
 int main()
 {
-
+    
 }
