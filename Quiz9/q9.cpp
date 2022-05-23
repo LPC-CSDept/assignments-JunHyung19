ced2c334019b859d7c0c37034af0f691e97ef899
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-23 13:41:49 -0700

    Quiz9

--- Quiz9/q9.cpp
+++ Quiz9/q9.cpp
@@ -13,10 +13,18 @@ struct employee
     double sal;//annual salaries
     string dep;//department name
     string date;//starting work date
-}
+};
 
+void printemployee(employee s);
+int makeStructArray(employee s[]);
+void findusername(employee s[],int numrec, string username);
 
 int main()
 {
-    
+    int numrec;
+    employee s[50];
+    string username;
+
+    numrec = makeStructArray(s);
+    for(int i=0;i<numrec;i++)
 }
