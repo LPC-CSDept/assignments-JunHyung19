0d7f5526f5d9f71ebca970b6c2262aa8126ac786
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-26 11:14:58 -0700

    final (copied prompt)

--- Final/f3.cpp
+++ Final/f3.cpp
@@ -5,7 +5,9 @@
 using namespace std;
 
 int main()
-
 {
-    
+    const int SIZE1=5;//copied from prompt
+    const int SIZE2=10;
+    int numbers1[SIZE1] = {2, 3, 5, 9, 10};
+    int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
 }
