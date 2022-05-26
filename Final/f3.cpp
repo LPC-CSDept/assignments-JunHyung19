28a196d5490216d1a7c9703718762e16e81a30dd
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-26 12:30:49 -0700

    final

--- Final/f3.cpp
+++ Final/f3.cpp
@@ -4,8 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int makearray(int numbers1[], int numbers2[]);
-void makearray(int numbers1[], int numbers2[]);
+void printArray(int numbers1[], int numbers2[]);
 
 int main()
 {
@@ -13,4 +12,6 @@ int main()
     const int SIZE2=10;
     int numbers1[SIZE1] = {2, 3, 5, 9, 10};
     int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+
+
 }
