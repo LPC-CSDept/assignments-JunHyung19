a63a94a799b8584f48cc737e7835e71d91357bd7
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-26 11:27:49 -0700

    final (copied prompt)

--- Final/f4.cpp
+++ Final/f4.cpp
@@ -1,30 +1,45 @@
 //Jonathan Park
 //Professor Lee
 //CS 1
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <fstream>
+#include    <iostream>
+#include    <fstream>
+#include    <iomanip>
 using namespace std;
 
 //copied from prompt
 const unsigned short N=5;
-struct Scores{     
-      double sc[N];     
-      string grade;
+struct  Scores{
+    double  sc[N];
+    string  grade;
 };
-
-struct Grade{    
-      string first;    
-      string last;   
-      string ssn;   
-      Scores score;
+struct Grade{
+    string  first;
+    string  last;
+    string  ssn;
+    Scores  score;
 };
-
 Grade g;
-
+void printstruct(Grade);
 int main()
-
 {
-    
+    ifstream    ifs;
+
+    ifs.open("grades.bin");
+    if(! ifs ){
+        cout << "File Open Error : Sure that there is the file grades.bin \n";
+        exit(0);
+    }
+
+    while ( ifs.read((char *)&g, sizeof(g))){
+        printstruct(g);
+    }
+}
+void printstruct(Grade g)
+{
+    cout << "Name : " << g.first << " " << g.last << "\t" << " SSN: " << g.ssn << "\n";
+    cout << "\tScores: " ;
+    for(int i=0; i<N;i++)
+        cout << showpoint << setprecision(2) << g.score.sc[i] << "\t" ;
+    cout << "Grade " << g.score.grade << endl;
+
 }
