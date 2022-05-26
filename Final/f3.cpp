670779813780055827431e7012152c8b247d8aea
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-26 12:33:14 -0700

    final

--- Final/f3.cpp
+++ Final/f3.cpp
@@ -13,6 +13,6 @@ int main()
     const int SIZE2=10;
     int numbers1[SIZE1] = {2, 3, 5, 9, 10};
     int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
-
-
 }
+
+void printArray(int numbers1[], int numbers2[], int SIZE1, int SIZE2);
