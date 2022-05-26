fa4cdd21add4bf4f89b3ec61e3da9e744116e6ad
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-26 11:22:27 -0700

    final (prompt copied)

--- Final/f4.cpp
+++ Final/f4.cpp
@@ -7,6 +7,22 @@
 #include <fstream>
 using namespace std;
 
+//copied from prompt
+const unsigned short N=5;
+struct Scores{     
+      double sc[N];     
+      string grade;
+};
+
+struct Grade{    
+      string first;    
+      string last;   
+      string ssn;   
+      Scores score;
+};
+
+Grade g;
+
 int main()
 
 {
