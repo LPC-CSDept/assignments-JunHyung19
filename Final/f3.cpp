97659b661d8b45b106b035c9ee5699e6b9069d3e
JunHyung19 <jxpark0031@zonemail.clpccd.edu>
2022-05-26 11:55:40 -0700

    final

--- Final/f3.cpp
+++ Final/f3.cpp
@@ -4,9 +4,10 @@
 #include <iostream>
 using namespace std;
 
+//copied from prompt
 int main()
 {
-    const int SIZE1=5;//copied from prompt
+    const int SIZE1=5;
     const int SIZE2=10;
     int numbers1[SIZE1] = {2, 3, 5, 9, 10};
     int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
